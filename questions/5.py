10bae8b282695a698387c52cfd83c0c667d91970
xiaowang <xiaowang@xiaowangs-iMac.local>
2016-02-18 17:42:39 +0800

    补充

--- questions/5.py
+++ questions/5.py
@@ -6,6 +6,9 @@ __date__ = '18/2/16'
 
 def multipliers():
     return [lambda x : i * x for i in range(4)]
+    # return [lambda x , i=i: i * x for i in range(4)]
+
+
 
 
 print([m(2) for m in multipliers()])
